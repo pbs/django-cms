8a846be8c092e81575fd3d457586659c8e1fe50a
pianas <paul.ianas@3pillarglobal.com>
2014-06-05 10:04:32 +0300

    Postpone version bump

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
-__version__ = '2.3.5pbs.14'
+__version__ = '2.3.5pbs.13'
 
 # patch settings
 try:
     from django.conf import settings
     if 'cms' in settings.INSTALLED_APPS:
-        from conf import patch_settings
+        from cms.conf import patch_settings
         patch_settings()
-except ImportError: # pragma: no cover
+except ImportError:  # pragma: no cover
     """
     This exception means that either the application is being built, or is
     otherwise installed improperly. Both make running patch_settings
     irrelevant.
     """
-    pass
-
