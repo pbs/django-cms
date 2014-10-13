fab4e98fd6e34e9458951491cd61183e60df87d7
Alex Pirjol <alexandru.pirjol@3pillarglobal.com>
2014-10-13 13:42:14 +0300

    LUN-1608: remove debug

--- cms/templatetags/iframe_HTML_decode.py
+++ cms/templatetags/iframe_HTML_decode.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 @register.filter
 def decode_iframe (string):
-    import ipdb; ipdb.set_trace()
     iframe_start = re.compile("&lt;iframe", re.IGNORECASE)
     iframe_end = re.compile("&gt;&lt;/iframe&gt;", re.IGNORECASE)
     string = iframe_start.sub("<iframe", string)
