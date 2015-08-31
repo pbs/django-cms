2761ebf745e37c1f32fd8ac64c811054007d362b
bogdan.virtosu <virtosu.bogdan@gmail.com>
2015-08-31 09:50:15 +0300

    LUN-2334 Changed cache deletion to improve performance.
    -replace multiple cache.delete with cache.delete_many
    -reduce the size of query result used to delete user cache keys

--- cms/cache/permissions.py
+++ cms/cache/permissions.py
@@ -14,8 +14,11 @@ PERMISSION_KEYS = [
 
 
 def get_cache_key(user, key):
+    # To optimize the ammout of data returned from the db, this can also be used only with
+    # the username. Previous usage (with the full user) has been left for compatibility.
+    username = user.username if hasattr(user, 'username') else user
     return "%s:permission:%s:%s" % (
-        settings.CMS_CACHE_PREFIX, user.username, key)
+        settings.CMS_CACHE_PREFIX, username, key)
 
 
 def get_permission_cache(user, key):
@@ -52,8 +55,9 @@ def clear_permission_cache():
         # Cache clearing is temporarily disabled, see
         # global_settings.CMS_DISABLE_SITE_CACHE_CLEAR for more info.
         return
-    users = User.objects.filter(is_active=True)
-    for user in users:
+    usernames = User.objects.filter(is_active=True).values_list('username', flat=True)
+    cache_keys_to_delete = []
+    for username in usernames:
         for key in PERMISSION_KEYS:
-            cache_key = get_cache_key(user, key)
-            cache.delete(cache_key)
+            cache_keys_to_delete.append(get_cache_key(username, key))
+    cache.delete_many(cache_keys_to_delete)
